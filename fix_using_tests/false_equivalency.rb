4638a8986c6d873a1e89b1e95e35048d7ca6e296
Javaria Brascom <javariab17@gmail.com>
2020-09-12 18:06:06 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,5 +1,5 @@
 # don't forget to add: require 'pry'
-require 'pry'
+
 def get_user_input
   gets.chomp
 end
@@ -13,10 +13,10 @@ end
 def selection(num)
   if num == 1
      "YUM YUM MUNCH MUNCH MUNCH"
-     binding.pry
+     
   elsif num == 2
      "HAM HAM HAM IN MY TUMMY"
-     binding.pry
+     
   end
 end
 
